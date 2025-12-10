968e3556356005ffd08e6c20c3381a8840b5b5cc
Ohmny Romero <36288190+ohmnoms@users.noreply.github.com>
2025-12-10 00:07:52 -0500

    feat: updated readme

--- backend/Todo.Api/Models/Contracts/GetTodoRequest.cs
+++ backend/Todo.Api/Models/Contracts/GetTodoRequest.cs
@@ -1,8 +1,11 @@
 namespace Todo.Api.Models.Contracts;
 
-public record GetTodoRequest {
+public record GetTodoRequest
+{
     /// <summary>
     /// Optional filter to get Todo items created by a specific user/device.
+    /// NOTE: This is currently enforced for security reasons, so you cannot access anyone else's Todo items.
+    /// Left as optional here for future extensibility.
     /// </summary>
     public Guid? CreatedBy { get; init; }
     /// <summary>
